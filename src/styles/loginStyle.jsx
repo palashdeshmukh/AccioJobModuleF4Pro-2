8373d7211592d6d9bf34f28fb9092fbed9f66bc4
palashdeshmukh <palash.deshmukh02@gmail.com>
2023-06-24 22:34:23 -0400

    Accio Job F4 Project 2

---
+++ src/styles/loginStyle.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const LogIn_Container=styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+width:100vw;
+height: 100vh;
+padding: 20px;
+box-sizing: border-box;
+background:rgba(0,0,0,0.9);
+`
